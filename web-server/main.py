ab90c076ca70bc31584147d1dad471565b24c00d
Ecastro08 <worldmechatronics2023@gmail.com>
2024-06-07 16:37:38 -0500

    Creando nuestro servidor web mediante fastapi

---
+++ web-server/main.py
@@ -0,0 +1,24 @@
+import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,4,5]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Soy Eduardo Castro</h1>
+        <P> este parrafo nos dice que la fastapi ademas de mostrar datos como listas, json, tambien permite contenido HTML</p>
+    """
+
+
+def run():
+    store.get_categories()
+
+
+if __name__ == '__main__':
+    run()
